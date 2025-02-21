fa2bf295475933c65be726fc5ad99c519b6efd79
marphic-digital <marphicdigital@gmail.com>
2025-02-21 20:39:50 +0500

    pizza-menu

--- pizza-menu/src/index.jsx
+++ pizza-menu/src/index.jsx
@@ -1,6 +1,51 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 function App() {
   return (
     <div className="container">
@@ -21,30 +66,51 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizzas />
-      <Pizzas />
-      <Pizzas />
-      <Pizzas />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizzas pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizzas
+        name="Pizza spinaci"
+        photoName="pizzas/spinaci.jpg"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={10}
+      /> */}
     </main>
   );
 }
-function Footer() {
+function Pizzas(props) {
   return (
-    <div className="footer">
-      <footer>{new Date().toLocaleTimeString()} We are open Now</footer>
-    </div>
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const open = 10;
+  const close = 22;
+  const isOpen = hour >= open && hour <= close;
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>
+            We are open from {open}AM to {close}PM.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
   );
 }
 
-function Pizzas() {
-  return (
-    <div className="pizza">
-      <img src="pizzas/spinaci.jpg" alt="pizza spinaci " />
-      <h3>Pizza spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
