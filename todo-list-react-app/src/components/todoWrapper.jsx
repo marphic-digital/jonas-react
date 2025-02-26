eca16982a245bca2bc91322337bd1702422f18bd
marphic-digital <marphicdigital@gmail.com>
2025-02-26 20:47:36 +0500

    todoList-complete

--- todo-list-react-app/src/components/todoWrapper.jsx
+++ todo-list-react-app/src/components/todoWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
 const TodoWrapper = () => {
@@ -18,22 +19,40 @@ const TodoWrapper = () => {
       )
     );
   };
-
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task: task, isEditing: false } : todo
+      )
+    );
   };
   return (
     <div className="TodoWrapper">
       <h1>Get Things done!</h1>
       <TodoForm addTodo={addTodo} />
-      {todos.map((todo, index) => (
-        <Todo
-          task={todo}
-          key={index}
-          toggleComplete={toggleComplete}
-          deleteTodo={deleteTodo}
-        />
-      ))}
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
     </div>
   );
 };
